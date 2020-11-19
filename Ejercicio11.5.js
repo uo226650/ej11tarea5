bfb63a8fbe024d3ac70e8bb942b832a8a3d496db
uo226650 <uo226650@uniovi.es>
2020-11-19 15:42:08 +0100

    dinamico maps

---
+++ Ejercicio11.5.js
@@ -0,0 +1,26 @@
+//Tarea 5: Crear un mapa dinámico con Google Maps indicando la posición del usuario
+// TAW y WAVE dan problemas de accesibilidad debido a la naturaleza de los mapas dinámicos de google
+// AIzaSyC8aUp4J8B-MOmk6mg4A8cSsbE3qzEpB1g (miKey)
+
+// Encapsulamiento de la función initMap como método de la case MapaDinamico
+class MapaDinamico{
+
+    initMap(){
+        navigator.geolocation.getCurrentPosition(this.success.bind(this));
+    }
+
+    success(posicion){
+        this.longitud = posicion.coords.longitude.toFixed(6); 
+        this.latitud = posicion.coords.latitude.toFixed(6);
+        console.log("Posicion cargada correctamente");
+    }
+
+    showMap(){
+        var favorito = {lat: Number(this.latitud) , lng: Number(this.longitud)};
+        debugger;
+        var mapaFavorito = new google.maps.Map(document.getElementById('mapa'),{zoom: 16,center:favorito});
+        var marcador = new google.maps.Marker({position:favorito,map:mapaFavorito});
+    }
+}
+
+var mapaDinamicoGoogle = new MapaDinamico();
